3e9c0d983498aa96b94bb415dec3ed6a5bf3c63e
Test <101502649+susamobus@users.noreply.github.com>
2023-12-22 16:24:25 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,9 +1,18 @@
 
-P.getOperatorIndex=function (i,i2=0){
+function Submit4() {
+ let input = document.getElementById("input1").value
+ let i = document.getElementById("input2").value
+ let i2 = document.getElementById("input3").value
+ let array = ProcessArray(input)
+ document.getElementById("debug").innerHTML = "1"
+ document.getElementById("text3").innerHTML = JSON.stringify(array)
+ document.getElementById("text1").innerHTML = getOperatorIndex(array,i,i2)
+}
+
+function getOperatorIndex(array,i,i2=0){
     if (typeof i!="number") i=Number(i);
     if (typeof i2!="number") i=Number(i2);
-    if (!isFinite(i)) throw Error(invalidArgument+"Index out of range.");
-    var a=this.array;
+    var a=array;
     var min=0,max=a.length-1;
     if (a[max][2]<i2) return max+0.5;
     if (a[min][2]>i2) return -0.5;
@@ -26,6 +35,7 @@ P.getOperatorIndex=function (i,i2=0){
     }
     return a[min][2]==i2&&a[min][0]==i?min:min+0.5;
   };
+/*
   P.getOperator=function (i,i2=0,i3=1){
     if (typeof i!="number") i=Number(i);
     if (typeof i2!="number") i=Number(i2);
@@ -35,3 +45,5 @@ P.getOperatorIndex=function (i,i2=0){
     if (Number.isInteger(ai)) return this.array[ai][i3];
     else return i===0&&i2===0?10:0;
   };
+  */
+
