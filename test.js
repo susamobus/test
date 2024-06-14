a9aa06dddfd33b2a515e74e6cf25c7b0ef5a469d
Test <101502649+susamobus@users.noreply.github.com>
2024-06-14 11:53:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -182,11 +182,10 @@ function normalize(array,layer){
       if (x.array.length>=2&&x.array[0][0]===0&&x.array[0][2]===0&&x.array[1][0]!=1&&x.array[0][1]!=10){
         if (x.array[0][1]) x.array.splice(1,0,[x.array[1][0]-1,x.array[0][1],x.array[1][2]]); //Upd
         x.array[0][1]=1;
-        const index = getOperatorIndex(x.array,x.array[1][0],x.array[1][2]);
-        if (x.array[index][1]>1){
-          x.array[index][1]--;
+        if (x.array[2][1]>1){
+          x.array[2][1]--;
         }else{
-          x.array.splice(index,1);
+          x.array.splice(1,2);
         }
         b=true;
       }
